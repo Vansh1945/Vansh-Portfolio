a07e0ba28a50e2d814ad4e074d3aa96fb435e0ed
Vansh <148086629+Vansh1945@users.noreply.github.com>
2026-07-07 23:28:33 +0530

    Update MongoDB connection URI handling

--- server/server.js
+++ server/server.js
@@ -77,7 +77,7 @@ app.use((err, req, res, next) => {
 });
 
 // Connect to MongoDB
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/portfolio';
+const MONGODB_URI = process.env.MONGODB_URI;
 mongoose
   .connect(MONGODB_URI)
   .then(() => {
